de6011f195ed96b2c5cd203083ae2628d837fe92
Khaled <kld.najjar@gmail.com>
2020-03-28 19:56:18 +0300

    Adding dashboard

--- src/views/pages/dashboard/index.js
+++ src/views/pages/dashboard/index.js
@@ -2,12 +2,20 @@ import React, { Component } from "react";
 import StayReservations from "../stay_reservations";
 import FoodReservations from "../food_reservations";
 
+import { Col, Row } from "reactstrap";
+
 class Dashboard extends Component {
   render() {
     return (
       <React.Fragment>
-        <StayReservations isDashboard="true" />
-        <FoodReservations isDashboard="true" />
+        <Row>
+          <Col>
+            <StayReservations isDashboard="true" />
+          </Col>
+          <Col>
+            <FoodReservations isDashboard="true" />
+          </Col>
+        </Row>
       </React.Fragment>
     );
   }
